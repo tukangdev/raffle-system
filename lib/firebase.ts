8911b1dc5408bb393e6c05a7b567ac2a5f77ef3f
Amin Roslan <amnrsln@gmail.com>
2020-11-24 23:48:03 +0800

    fix env

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,7 +4,7 @@ try {
   admin.initializeApp({
     credential: (admin.credential as any).cert({
       project_id: process.env.FIREBASE_PROJECT_ID,
-      private_key: process.env.FIREBASE_PRIVATE_KEY,
+      private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
       client_email: process.env.FIREBASE_CLIENT_EMAIL,
     }),
     databaseURL: "https://raffledaffle-2e9e1.firebaseio.com",
